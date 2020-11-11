9fe288ce9f2ece4445bf08ac1682e8432914d3ad
Leo <leo.chen@cloud-interactive.com>
2020-11-11 15:11:20 +0800

    add basic data

--- index.js
+++ index.js
@@ -1,32 +1,104 @@
 const { ApolloServer, gql } = require('apollo-server');
-// ApolloServer: 讓我們啟動 server 的 class ，不但實作許多 GraphQL 功能也提供 web application 的功能 (背後使用 express)
-// gql: template literal tag, 讓你在 Javascript 中使用 GraphQL 語法
 
-// 1. GraphQL Schema 定義
+// Schema
 const typeDefs = gql`
+  type User {
+    id: Int!
+    email: String!
+    name: String
+    age: Int
+    friends: [User]
+    posts: [Post]
+  }
+
+  type Post {
+    id: Int!
+    author: User
+    title: String
+    description: String
+    likeUsers: [User]
+    createdAt: String
+  }
+
   type Query {
-    "A simple type for getting started!"
-    hello: String
+    self: User
+    users: [User]
+    user(id: Int!): User
+    posts: [Post]
+    post(id: Int!): Post
   }
 `;
 
-// 2. Resolvers 是一個會對照 Schema 中 field 的 function map ，讓你可以計算並回傳資料給 GraphQL Server
+const meId = 1;
+const users = [
+  {
+    id: 1,
+    email: 'leo.chen@test.com',
+    password: 'leo',
+    name: 'leo',
+    age: 20,
+    friendIds: [2, 3]
+  },
+  {
+    id: 2,
+    email: 'woody@test.com',
+    password: 'woody',
+    name: 'Woody',
+    age: 30,
+    friendIds: [1]
+  },
+  {
+    id: 3,
+    email: 'ding@test.com',
+    password: 'ding',
+    name: 'Ding',
+    age: 40,
+    friendIds: [1]
+  }
+];
+
+const posts = [
+  {
+    id: 1,
+    authorId: 1,
+    title: 'Hello World',
+    description: 'This is my first post',
+    likeUsers: [1, 2],
+    createdAt: '2018-10-22T01:40:14.941Z'
+  },
+  {
+    id: 2,
+    authorId: 2,
+    title: 'Nice Day',
+    description: 'Hello My Friend!',
+    likeUsers: [1],
+    createdAt: '2018-10-24T01:40:14.941Z'
+  }
+];
+
+// Resolvers
 const resolvers = {
   Query: {
-    // 需注意名稱一定要對到 Schema 中 field 的名稱
-    hello: () => 'world'
+    self: () => users.find(user => user.id === meId),
+    users: () => users,
+    user: (root, args) => users.find(user => user.id === args.id),
+    posts: () => posts,
+    post: (root, args) => posts.find(post => post.id === args.id)
+  },
+  User: {
+    posts: (parent) => posts.filter(post => post.authorId === parent.id),
+    friends: (parent) => users.filter(user => parent.friendIds.includes(user.id))
+  },
+  Post: {
+    author: (parent) => users.find(user => user.id === parent.authorId),
+    likeUsers: (parent) => parent.likeUsers.map(id => users.find(user => user.id === id))
   }
 };
 
-// 3. 初始化 Web Server ，需傳入 typeDefs (Schema) 與 resolvers (Resolver)
-const server = new ApolloServer({
-  // Schema 部分
-  typeDefs,
-  // Resolver 部分
-  resolvers
-});
+// 初始化 Web Server，需傳入 typeDefs (Schema) 與 resolvers (Resolver)
+const server = new ApolloServer({ typeDefs,resolvers});
 
 // 4. 啟動 Server
 server.listen().then(({ url }) => {
-  console.log(`? Server ready at ${url}`);
+  console.log(`🚀 Server ready at ${url}`);
 });
